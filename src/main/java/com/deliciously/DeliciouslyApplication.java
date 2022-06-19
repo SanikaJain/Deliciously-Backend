f1464ab56501ba74a8634dee9fe3fabb6a73e0f5
godzilla <47269389+SanikaJain@users.noreply.github.com>
2022-06-19 15:42:34 +0530

    Adding Initial Project

---
+++ src/main/java/com/deliciously/DeliciouslyApplication.java
@@ -0,0 +1,13 @@
+package com.deliciously;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciouslyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciouslyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliciously/DeliciouslyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliciously;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciouslyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
